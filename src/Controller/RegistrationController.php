cf3ebf31e645a0e9c242f41c886ed49912e989e7
Sami433 <84328801+Sami433@users.noreply.github.com>
2022-08-01 23:18:35 +0200

    Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,7 +49,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('azzzzzertyyyyy2@gmail.com', 'sami'))
+                    ->from(new Address('mail', 'nom'))
                     ->to($user->getEmail())
                     ->subject('Bienvenue!')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
